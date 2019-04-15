8ce9cf345fb383fc7eb8502a4c8ed3ff86b54fc8
Taylor Buchanan <me+dev.git@taylorbuchanan.com>
2019-04-15 13:08:04 -0500

    Preserve Gravatar size during load

--- src/NetworkProfile.js
+++ src/NetworkProfile.js
@@ -12,7 +12,7 @@ function NetworkProfile(props) {
             <NetworkGroup className="professional" title="Professional"
                           networks={professionalNetworks} titlePosition="top" />
 
-            <img title="George Taylor Buchanan" alt="George Taylor Buchanan" src="https://www.gravatar.com/avatar/bab942d497bfde94e541a3d6fc299202?s=250" />
+            <img title="George Taylor Buchanan" alt="George Taylor Buchanan" src="https://www.gravatar.com/avatar/bab942d497bfde94e541a3d6fc299202?s=250" width="250" height="250" />
 
             <NetworkGroup className="social" title="Social"
                           networks={socialNetworks} titlePosition="bottom" />
