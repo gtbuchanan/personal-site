e758703f239d9ac048d8c544885c81917ac7d4b1
Taylor Buchanan <me+dev.git@taylorbuchanan.com>
2024-03-31 22:29:15 -0500

    Remove Google Analytics

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactGA from 'react-ga';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactGA.initialize('UA-60694538-1');
-ReactGA.pageview(window.location.pathname + window.location.search);
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
